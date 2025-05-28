9afca783496c8f7c6f815214b849c0466fc43ed8
KEERTHIKUMAR517 <keerthikumar517@gnail.com>
2025-05-28 20:17:04 +0100

    code release

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import os
+import boto3
+from transformers import pipeline
+import torch
+
+
+bucket_name = 'mlops-bucket-2025'
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+local_path = 'tiny-bert-streamlit'
+s3_prefix = 'ml-models/tinybert-sentiment-analysis'
+
+s3 = boto3.client('s3')
+def download_dir(local_path, s3_prefix):
+    os.makedirs(local_path, exist_ok=True)
+    paginator = s3.get_paginator('list_objects_v2')
+    for result in paginator.paginate(Bucket=bucket_name, Prefix=s3_prefix):
+        if 'Contents' in result:
+            for key in result['Contents']:
+                s3_key = key['Key']
+
+                local_file = os.path.join(local_path, os.path.relpath(s3_key, s3_prefix))
+                # os.makedirs(os.path.dirname(local_file), exist_ok=True)
+
+                s3.download_file(bucket_name, s3_key, local_file)
+
+
+st.title('ML model Deployment')
+button = st.button('Download model')
+if button:
+    with st.spinner('Downloading... PLzz wait'):
+        download_dir(local_path, s3_prefix)
+        st.write('Model Downloaded')
+
+text = st.text_area('Enter your review','Typre here')
+
+predict = st.button('Predict')
+classifier = pipeline('text-classification', model='tiny-bert-streamlit', device=device)
+
+if predict:
+    with st.spinner('Predicting output...'):
+        output = classifier(text)
+
+        st.write(output)
+      #  st.info(output)
