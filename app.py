b4b2ebfe2d908f1162fdb6fe9561ff79786cef4d
KEERTHIKUMAR517 <94115016+KEERTHIKUMAR517@users.noreply.github.com>
2025-05-28 21:52:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
+## ML Model Deployment at Streamlit Server
+# Full Streamlit Code Repository: https://github.com/laxmimerit/streamlit-tutorials
+
+# streamlit run 2-app.py
+
 import streamlit as st
 import os
-import boto3
-from transformers import pipeline
 import torch
+from transformers import pipeline
 
-# Load from Streamlit secrets
-aws_access_key = st.secrets["AWS_ACCESS_KEY_ID"]
-aws_secret_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
-
+import boto3
 bucket_name = 'mlops-bucket-2025'
-device = 0 if torch.cuda.is_available() else -1  # HuggingFace expects int, not torch.device
 
-local_path = 'tiny-bert-streamlit'
+local_path = 'tinybert-sentiment-analysis'
 s3_prefix = 'ml-models/tinybert-sentiment-analysis/'
 
-# Use secrets to create S3 client
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=aws_access_key,
-    aws_secret_access_key=aws_secret_key
-)
-
+s3 = boto3.client('s3')
 def download_dir(local_path, s3_prefix):
     os.makedirs(local_path, exist_ok=True)
     paginator = s3.get_paginator('list_objects_v2')
@@ -28,25 +22,29 @@ def download_dir(local_path, s3_prefix):
         if 'Contents' in result:
             for key in result['Contents']:
                 s3_key = key['Key']
-                if s3_key.endswith('/'):
-                    continue  # Skip folders
+
                 local_file = os.path.join(local_path, os.path.relpath(s3_key, s3_prefix))
-                os.makedirs(os.path.dirname(local_file), exist_ok=True)
+                # os.makedirs(os.path.dirname(local_file), exist_ok=True)
+
                 s3.download_file(bucket_name, s3_key, local_file)
 
-st.title('ML Model Deployment')
-button = st.button('Download model')
 
+st.title("Machine Learning Model Deployment at the Server!!!")
+
+button = st.button("Download Model")
 if button:
-    with st.spinner('Downloading... Please wait'):
+    with st.spinner("Downloading... Please wait!"):
         download_dir(local_path, s3_prefix)
-        st.success('Model Downloaded ✅')
 
-text = st.text_area('Enter your review', 'Type here')
-predict = st.button('Predict')
 
+text = st.text_area("Enter Your Review", "Type...")
+predict = st.button("Predict")
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+classifier = pipeline('text-classification', model='tinybert-sentiment-analysis', device=device)
 if predict:
-    with st.spinner('Predicting output...'):
-        classifier = pipeline('text-classification', model=local_path, device=device)
+    with st.spinner("Predicting..."):
         output = classifier(text)
         st.write(output)
+        # st.info(output)
+
